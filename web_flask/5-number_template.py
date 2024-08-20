56dd6d970e3751d5b6b954ec6bb1e47abe386633
Sohila Hamdy <sohila.fathy22@gmail.com.com>
2024-08-20 14:27:18 +0000

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Module for Flask routes"""
-
-from flask import Flask, render_template, abort
+"""
+A simple Flask web application with multiple routes
+"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
@@ -20,31 +21,27 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """Displays 'C ' followed by the value of text,
-    replacing underscores with spaces"""
-    text = text.replace('_', ' ')
-    return "C {}".format(text)
+    """Displays 'C ' followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """Displays 'Python ' followed by the value of text,
-    replacing underscores with spaces"""
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
+def python_route(text):
+    """Displays 'Python ' followed by the value of the text variable"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
+def number(n):
     """Displays 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page only if n is an integer"""
-    return render_template('5-number.html', number=n)
+    """Displays an HTML page with 'Number: n'"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
